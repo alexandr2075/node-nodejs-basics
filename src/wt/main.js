8f5a2a90a112177a43c74e2ee27dde7f1be13695
alexandr2075 <gagara5620@gmail.com>
2023-06-16 20:42:44 +0600

    feat:  worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = path.join(__dirname, 'worker.js');
+const cpus = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  for (let index = 0; index < cpus; index++) {
+    const worker = new Worker(pathToFile);
+    worker.postMessage(10 + index);
+    worker.on('message', (message) => {
+      console.log(message);  
+    });
+    worker.on("error", err => console.error('terror: ', err));
+  }
+    
 };
 
 await performCalculations();
